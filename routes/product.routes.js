a68d3eb83e4f3dfd8d65913124627b5371260754
TomasWielandt <twielandt16@gmail.com>
2024-09-02 17:27:57 -0400

    se crea el archivo product.routes.js

---
+++ routes/product.routes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+// const auth = require('../middleware/authorization');
+
+const { readall, readone, create, update, remove } = require('../controllers/product.controller');
+
+router.get("/readall", readall);
+router.get("/readone/:id", readone);
+router.post("/create", create);
+router.put("/update/:id", update);
+router.delete("/remove/:id", remove);
+
+module.exports = router;

a0fcaaf8942ee1a86cf0a689d0536e447687504d
TomasWielandt <twielandt16@gmail.com>
2024-09-03 17:07:50 -0400

    se crea archivo swagger.js

---
+++ swagger.js
@@ -0,0 +1,28 @@
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+swaggerDefinition = {
+    openapi: '3.0.0',
+    info: {
+        title: 'Aplicación Backend con Autenticación',
+        version: '1.0.0',
+        description: 'Documentación de la API del proyecto 6',
+    },
+    servers: [
+        {
+            url: 'http://localhost:3000',
+            description: 'Servidor local',
+        },
+    ],
+};
+
+const options = {
+    swaggerDefinition,
+    apis: ['./routes/*.js'],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+module.exports = (app) => {
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+};

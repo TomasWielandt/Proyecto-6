1b48d66b97671f5cc2344616c1a9b6e940e11060
TomasWielandt <twielandt16@gmail.com>
2024-09-03 13:24:34 -0400

    se agrega authRouter a index.js

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const userRouter = require('./user.routes');
-// const authRouter = require('./auth.routes');
+const authRouter = require('./auth.routes');
 const productRouter = require('./product.routes');
 
 router.use('/user', userRouter);
-// router.use('/', authRouter);
+router.use('/user', authRouter);
 router.use('/product', productRouter);
 
 module.exports = router;

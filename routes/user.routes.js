f63e1fae49c47f4ddba193e0f9c81249bef68bf0
TomasWielandt <twielandt16@gmail.com>
2024-09-03 13:38:59 -0400

    se agrega auth a user.router.js y product.router.js

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-// const auth = require('../middleware/authorization');
+const auth = require('../middleware/authorization');
 
 const { readall, readone, create, update, remove } = require('../controllers/user.controller');
 
-router.get("/readall", readall);
-router.get("/readone/:id", readone);
-router.post("/create", create);
-router.put("/update/:id", update);
-router.delete("/remove/:id", remove);
+router.get("/readall", auth, readall);
+router.get("/readone/:id", auth, readone);
+router.post("/create", auth, create);
+router.put("/update/:id", auth, update);
+router.delete("/remove/:id", auth, remove);
 
 module.exports = router;

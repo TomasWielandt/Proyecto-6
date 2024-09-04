710e57dd308feb898574166e880e1c6176a08163
TomasWielandt <twielandt16@gmail.com>
2024-09-04 16:12:25 -0400

    se cambió el orden de las rutas, se puso create primero en product.routes.js

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -5,9 +5,9 @@ const auth = require('../middleware/authorization');
 
 const { readAll, readOne, create, update, remove } = require('../controllers/product.controller');
 
+router.post("/create", auth, create);
 router.get("/readall", auth, readAll);
 router.get("/readone/:id", auth, readOne);
-router.post("/create", auth, create);
 router.put("/update/:id", auth, update);
 router.delete("/delete/:id", auth, remove);
 

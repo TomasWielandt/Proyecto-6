ad5b62ee6ea554fdd1988fa44a929cec2fc54457
TomasWielandt <twielandt16@gmail.com>
2024-09-04 12:15:44 -0400

    se agrega swagger para login en auth.routes.js

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -54,8 +54,41 @@ const { register, logIn } = require('../controllers/auth.controller');
  *       500:
  *         description: Error al dar de alta al usuario
  */
-
 router.post("/register", register);
+
+/**
+ * @swagger
+ * /api/user/login:
+ *   post:
+ *     summary: Iniciar sesión
+ *     description: Autenticar y autorizar ingreso a la sesión del usuario.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *                 example: juan@gmail.com
+ *               password:
+ *                 type: string
+ *                 example: 1234
+ *     responses:
+ *       200:
+ *         description: Inicio de sesión exitoso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 token:
+ *                   type: string
+ *                   example: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjZkNzQ2YzVlMTFiNTIwMTY2ZGUzMDFmIn0sImlhdCI6MTcyNTM4NDUwOCwiZXhwIjoxNzI4OTg0NTA4fQ.6oYXp4fYFKYq9qiVH1i29USDipKQvLkqLKtpymR8k0g
+ *       500:
+ *         description: Error al iniciar sesión del usuario
+ */
 router.post("/login", logIn);
 
 module.exports = router;

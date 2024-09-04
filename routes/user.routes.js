0a794324852df0c5ef5255896befb012ae8e6a18
TomasWielandt <twielandt16@gmail.com>
2024-09-04 16:10:01 -0400

    se cambió el orden de las rutas, se puso create segundo en user.routes.js

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -42,6 +42,61 @@ router.get("/verifytoken", auth, verifyToken);
 
 /**
  * @swagger
+ * /api/user/create:
+ *   post:
+ *     summary: Registrar un nuevo usuario
+ *     description: Añade un nuevo usuario a la base de datos.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 example: Juan
+ *               username:
+ *                 type: string
+ *                 example: juan@gmail.com
+ *               password:
+ *                 type: string
+ *                 example: 1234
+ *               active:
+ *                 type: boolean
+ *                 example: true
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Usuario creado exitosamente
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: string
+ *                   example: 66d746c5e11b520166de301k
+ *                 name:
+ *                   type: string
+ *                   example: Juan
+ *                 username:
+ *                   type: string
+ *                   example: juan@gmail.com
+ *                 password:
+ *                   type: string
+ *                   example: 1234
+ *                 active:
+ *                   type: boolean
+ *                   example: true
+ *       500:
+ *         description: Error creando un usuario
+ */
+router.post("/create", auth, create);
+
+/**
+ * @swagger
  * /api/user/readall:
  *   get:
  *     summary: Obtener usuarios
@@ -120,61 +175,6 @@ router.get("/readall", auth, readAll);
  *         description: Error obteniendo al usuario
  */
 router.get("/readone/:id", auth, readOne);
-
-/**
- * @swagger
- * /api/user/create:
- *   post:
- *     summary: Registrar un nuevo usuario
- *     description: Añade un nuevo usuario a la base de datos.
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               name:
- *                 type: string
- *                 example: Juan
- *               username:
- *                 type: string
- *                 example: juan@gmail.com
- *               password:
- *                 type: string
- *                 example: 1234
- *               active:
- *                 type: boolean
- *                 example: true
- *     security:
- *       - bearerAuth: []
- *     responses:
- *       200:
- *         description: Usuario creado exitosamente
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 id:
- *                   type: string
- *                   example: 66d746c5e11b520166de301k
- *                 name:
- *                   type: string
- *                   example: Juan
- *                 username:
- *                   type: string
- *                   example: juan@gmail.com
- *                 password:
- *                   type: string
- *                   example: 1234
- *                 active:
- *                   type: boolean
- *                   example: true
- *       500:
- *         description: Error creando un usuario
- */
-router.post("/create", auth, create);
 
 /**
  * @swagger
